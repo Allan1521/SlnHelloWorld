1411a9f0300a647a44efb70fffdb67d7443fd5b2
Allan <allan1521lima@gmail.com>
2024-09-05 22:18:06 -0300

    Tipos da dados, Getter and Setters

---
+++ PrjHelloWord/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//C# é uma linguagem compilada e tipada
+
+//int - números inteiros
+//int variavelNumerica = 2;
+
+//double variavelNumericaDecimal = 3.0;
+//double - números decimais
+
+//string - textos
+//string nome = "Allan";
+
+//bool - booleano(true/false)
+//bool temArCondicionado = false;
+
+//char - único caracter('usa-se apóstos')
+//char caracterUnico = 'S';
+
+using PrjHelloWord.models;
+using System.Reflection.Metadata;
+
+Pessoa minhaPessoa = new Pessoa();
+//Definindo uma variável como tipo pessoa p/armazenar um 
+//objeto pessoa. Nenhum tipo vai funcionar
+
+Console.WriteLine("Hello, Digite seu nome:");
+minhaPessoa.Nome = Console.ReadLine();
+Console.WriteLine("hello, Digite sua idade:");
+minhaPessoa.Idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"O nome da pessoa é {minhaPessoa.Nome} e a idade {minhaPessoa.Idade} anos.");
+
+
+
+
+
+
+
---
+++ PrjHelloWord/models/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace é aqui que construimos pacotes, onde vc organiza a classe. 
+//Seria o nome completo da sua classe.
+//E é comum usar namespace em qlqr linguagem
+namespace PrjHelloWord.models
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa() // Com o atalho CTO  acessa o método construtor padrão no C# e no Java tbém.
+        {
+            
+        }
+
+    }
+}
